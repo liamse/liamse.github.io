ee9a4925287f69e30490573f144c246bcba9baf2
liamse <esmuilsa@yahoo.com>
2017-05-13 14:24:54 +0430

    change sw.js place

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ var urlsToCache = [
 //Register Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('js/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
       	//Install Service Worker
